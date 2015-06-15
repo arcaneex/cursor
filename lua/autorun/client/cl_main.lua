5d4e53874f369b1f299f4533fde2ee8e0fdedfd5
arcaneex <epic@funny.cat>
2015-06-15 20:11:24 +0300

    Create cl_main.lua

---
+++ lua/autorun/client/cl_main.lua
@@ -0,0 +1,29 @@
+local function cursorDraw()
+
+	if not cursor.mouseEnabled then return end
+
+	local left,right = input.IsMouseDown(MOUSE_LEFT),input.IsMouseDown(MOUSE_RIGHT)
+
+	local x,y = gui.MousePos()
+	if left and cursor['left_click'] then
+
+		cursor['left_click'](x,y,CurTime())
+	elseif right and cursor['right_click'] then
+
+		cursor['right_click'](x,y,CurTime())
+	elseif cursor['default'] then
+
+		cursor['default'](x,y,CurTime())
+	end
+end
+
+local function cursorThink()
+
+	local hover = vgui.GetHoveredPanel()
+	if not IsValid(hover) then return end
+	
+	hover:SetCursor('blank')
+end
+
+hook.Add('HUDPaint','Draw cursor',cursorDraw)
+hook.Add('Think','Cursor think',cursorThink)
